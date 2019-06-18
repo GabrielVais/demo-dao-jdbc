84e831ff307a0d1031b58070dc36667c8438ba68
gabrielVais <gabriel_lol2017@hotmail.com>
2019-06-18 20:21:45 -0300

    inicio do projeto

---
+++ src/db/DB.java
@@ -0,0 +1,129 @@
+package db;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class DB {
+	
+	//conectando ao banco de dados
+	
+	private static Connection conn = null;
+	
+	
+	public static Connection getConnection() {
+		
+		if(conn == null) {
+			
+			try {
+			
+			Properties props = laodProperties();
+			
+			String url = props.getProperty("dburl");
+			
+			conn = DriverManager.getConnection(url, props);
+			
+			}
+			catch(SQLException e) {
+				
+				throw new DbException(e.getMessage());
+				
+			}
+			
+			
+		}
+		
+		return conn;
+		
+	}
+	
+	//fechando conexao
+	
+	public static void closeConnection() {
+		
+		if(conn != null) {
+			
+			try {
+			
+			conn.close();
+			
+			}
+			catch(SQLException e) {
+				
+				throw new DbException(e.getMessage());
+				
+			}
+			
+			
+		}
+		
+		
+	}
+	
+	private static Properties laodProperties() {
+		
+		
+		try(FileInputStream fs = 
+				new FileInputStream("db.properties")){
+			
+			Properties props = new Properties();
+			
+			props.load(fs);
+			return props;
+			
+		}
+		catch(IOException e) {
+			
+			throw new DbException(e.getMessage());
+			
+			
+		}
+		
+	}
+	
+	// -----METODO AUCILIAR PARA FECHAR CONEXAO
+	
+	public static void closeStatement(Statement stmt) { 
+		
+		
+		if(stmt != null) {
+			
+			try {
+			
+			stmt.close();
+			
+			}
+			catch(SQLException e) {
+				
+				throw new DbException(e.getMessage());
+				
+			}
+			
+		}
+		
+	}
+	
+	public static void closeResultSet(ResultSet rs) {
+
+		if (rs != null) {
+
+			try {
+
+				rs.close();
+
+			} catch (SQLException e) {
+
+				throw new DbException(e.getMessage());
+
+			}
+
+		}
+
+	}
+	
+
+}
