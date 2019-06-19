3ebf9265e2cf5aa88b50ac69d04a0f788c05d555
gabrielVais <gabriel_lol2017@hotmail.com>
2019-06-18 21:48:10 -0300

    implementado interfaces Daofactory

---
+++ src/model/dao/impl/SellerDAOJDBC.java
@@ -0,0 +1,40 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import mode.entities.Seller;
+import model.dao.SellerDAO;
+
+public class SellerDAOJDBC implements SellerDAO{
+
+	@Override
+	public void insert(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public SellerDAO findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<SellerDAO> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
